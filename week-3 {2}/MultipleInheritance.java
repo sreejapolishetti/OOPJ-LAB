f4e0e6d272c702863711c5b1ff3d5f717f9c1e26
sreejapolishetti <sreejapolishetti@gmail.com>
2026-02-25 12:30:30 +0530

    Add files via upload

---
+++ week-3 {2}/MultipleInheritance.java
@@ -0,0 +1,46 @@
+class student {
+    int rollNumber;
+    void getNumber(int n) {
+        rollNumber = n;
+    }
+    void printNumber() {
+        System.out.println("Roll Number: " + rollNumber);
+    }
+}
+class test extends student {
+    float part1, part2;
+    void getMarks(float a, float b) {
+        part1 = a;
+        part2 = b;
+    }
+    void putMarks() {
+        System.out.println("Marks Obtained:");
+        System.out.println("Part 1: " + part1);
+        System.out.println("Part 2: " + part2);
+    }
+}
+interface sports {
+    float sportwt = 6.0F;
+    void putwt();
+}
+class results extends test implements sports {
+    float total;
+    public void putwt() {
+        System.out.println("Sports Weightage: " + sportwt);
+    }
+    void display() {
+        total = part1 + part2 + sportwt;
+        System.out.println("Total Marks: " + total);
+    }
+}
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        results a = new results();
+        a.getNumber(101);
+        a.printNumber();
+        a.getMarks(85.5F, 90.0F);
+        a.putMarks();
+        a.putwt();
+        a.display();
+    }
+}
